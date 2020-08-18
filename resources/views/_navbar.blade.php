1cd3c8176cf4c7ef07d4aa76af6db5dc18234b2a
Minor García <magm1581@gmail.com>
2020-08-17 18:57:26 -0600

    side-menu

--- resources/views/_navbar.blade.php
+++ resources/views/_navbar.blade.php
@@ -1,11 +1,11 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm fixed-top">
+<nav class="navbar navbar-expand-md shadow-sm fixed-top">
     <div class="container">
         @guest
             <a class="navbar-brand" href="{{ url('/') }}">
                 Track Your Money
             </a>
         @else
-        <a class="navbar-brand" href="{{ url()->route('home') }}">
+        <a class="navbar-brand text-white" href="{{ url()->route('home') }}">
             Track Your Money
         </a>
         @endguest
@@ -33,12 +33,12 @@
                     @endif
                 @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="navbarDropdown" class="nav-link dropdown-item text-white dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
+                            <a class="dropdown-item text-white" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
--- resources/views/_sidebar-menu.blade.php
+++ resources/views/_sidebar-menu.blade.php
@@ -1,17 +1,38 @@
 <ul>
     <li>
-        <a href="" class="dropdown-item btn btn-link bg-transparent text-white text-xl text-bold">Currency</a>
+        <a href="" class="dropdown-item btn btn-link text-white text-xl text-bold mb-5 flex items-center rounded-full">
+            <img src="/images/coins.svg" alt="currency" width="50" height="50" class="mr-2">
+            <p>Currency</p>
+        </a>
     </li>
     <li>
-        <a href="" class="dropdown-item btn btn-link bg-transparent text-white text-xl text-bold">Accounts</a>
+        <a href="" class="dropdown-item btn btn-link text-white text-xl text-bold mb-5 flex items-center rounded-full">
+            <img src="/images/account.svg" alt="account" width="50" height="50" class="mr-2">
+            <p>Accounts</p>
+        </a>
     </li>
     <li>
-        <a href="" class="dropdown-item btn btn-link bg-transparent text-white text-xl text-bold">Categories</a>
+        <a href="" class="dropdown-item btn btn-link text-white text-xl text-bold mb-5 flex items-center rounded-full">
+            <img src="/images/categories.svg" alt="categories" width="50" height="50" class="mr-2">
+            <p>Categories</p>
+        </a>
     </li>
     <li>
-        <a href="" class="dropdown-item btn btn-link bg-transparent text-white text-xl text-bold">Add Income</a>
+        <a href="" class="dropdown-item btn btn-link text-white text-xl text-bold mb-5 flex items-center rounded-full">
+            <img src="/images/addIncome.svg" alt="addIncome" width="50" height="50" class="mr-2">
+            <p>Add Income</p>
+        </a>
     </li>
     <li>
-        <a href="" class="dropdown-item btn btn-link bg-transparent text-white text-xl text-bold">Add Expense</a>
+        <a href="" class="dropdown-item btn btn-link text-white text-xl text-bold mb-5 flex items-center rounded-full">
+            <img src="/images/addExpense.svg" alt="addExpense" width="50" height="50" class="mr-2">
+            <p>Add Expense</p>
+        </a>
+    </li>
+    <li>
+        <a href="" class="dropdown-item btn btn-link text-white text-xl text-bold mb-5 flex items-center rounded-full">
+            <img src="/images/transfer.svg" alt="transfer" width="50" height="50" class="mr-2">
+            <p>Transfer</p>
+        </a>
     </li>
 </ul>
